66cbe4e818cb3b612698a0577d0b5c09e4e43eb3
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2022-06-03 10:48:04 +0200

    Extend API Status

--- k8s-api/api/v1alpha1/longoperation_types.go
+++ k8s-api/api/v1alpha1/longoperation_types.go
@@ -67,7 +67,8 @@ const (
 type LongOperationStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	State LongOperationState `json:"state,omitempty"`
+	State              LongOperationState `json:"state,omitempty"`
+	ObservedGeneration int64              `json:"observedGeneration,omitempty"`
 }
 
 //+kubebuilder:object:root=true
